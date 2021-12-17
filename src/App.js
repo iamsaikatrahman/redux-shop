61f3344a5f5a85f65c7f99542ff9e7c9b0d89229
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2021-12-17 17:49:58 +0600

    add dispay product details

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./containers/Header";
 import ProductDetails from "./containers/ProductDetails";
 import ProductListing from "./containers/ProductListing";
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<ProductListing />} />
-        <Route path="/product/:productId" element={<ProductDetails />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductListing />} />
+          <Route path="/product/:productId" element={<ProductDetails />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
